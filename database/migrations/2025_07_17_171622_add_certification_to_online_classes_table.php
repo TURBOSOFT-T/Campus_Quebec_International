86d6bc9870d775317ca8dcd79ad810d463f19486
TURBOSOFT <42888421+TURBOSOFT-T@users.noreply.github.com>
2025-07-17 20:35:26 +0200

    update

--- database/migrations/2025_07_17_171622_add_certification_to_online_classes_table.php
+++ database/migrations/2025_07_17_171622_add_certification_to_online_classes_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
     {
         Schema::table('certifications', function (Blueprint $table) {
               $table->dropColumn('autre_description');
-                $table->dropColumn('autre_description');
+                $table->dropColumn('autre_description1');
 
         });
     }
